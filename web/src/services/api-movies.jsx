4e986ca38b583268b676a862a7303c5be86c819a
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-12-16 11:25:09 +0100

    template engines 1, 2 and 3

--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -3,7 +3,7 @@
 const getMoviesFromApi = () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch(' http://localhost:5000/movies')
+  return fetch(' http://localhost:3000/movies')
     .then(response => response.json())
     .then(data => {
       console.log(data.movies);
